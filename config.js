e9e8cdf4c05b7cc7de3675babc431600c8a7fa03
manishasasasmitha <ganegamawattemanishasasmitha@gmail.com>
2024-10-30 17:47:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //IPcUWIKQ#_NETaDs-5R1SaS5byteJT0bUslnkXPSuXKPmIryRkUc
+SESSION_ID: process.env.SESSION_ID || "Gf5m0DLR#aCCHLWrJltTvqjxt17wo4RtYjY1VrQCNGrin257ECyY", //IPcUWIKQ#_NETaDs-5R1SaS5byteJT0bUslnkXPSuXKPmIryRkUc
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
