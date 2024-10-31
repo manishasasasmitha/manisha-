5f8b080c976154abe9b022335b2ae6668641666d
manishasasasmitha <ganegamawattemanishasasmitha@gmail.com>
2024-10-31 15:22:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-😪const fs = require('fs');
+const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Gf5m0DLR#aCCHLWrJltTvqjxt17wo4RtYjY1VrQCNGrin257ECyY", //IPcUWIKQ#_NETaDs-5R1SaS5byteJT0bUslnkXPSuXKPmIryRkUc
+SESSION_ID: process.env.SESSION_ID || "RzEwQAoI#uHv76EUkrYuKygCsvSAQ2gjSJIXCdhw0MnDjk__ADzY", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
